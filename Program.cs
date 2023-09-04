5337b876daff68504730515b46902f8c3acc1bdf
duvanrg <135860225+duvanrg@users.noreply.github.com>
2023-09-04 12:28:44 -0500

    feat: :zap: coding operators
    coding opertaros fo the csharp
    
    @duvanrg

--- Program.cs
+++ Program.cs
@@ -3,11 +3,58 @@
     private static void Main(string[] args)
     {
         int edad;
-        Console.WriteLine("Ingrese su edad");
-        try{
-            edad = int.Parse(s: Console.ReadLine() ?? "0");
+        float precio = 23;
+
+        /* try{
+            Console.WriteLine("Ingrese su edad: ");
+            edad = int.Parse(s: Console.ReadLine());
+            Console.WriteLine("Ingrese el precio: ");
+            precio = float.Parse(Console.ReadLine());
         }catch (Exception ex){
             Console.WriteLine("Error {0}", ex.Message.ToString());
+        } */
+
+        /* // if simple
+        if (4 > 5)
+        {
+
         }
+        // if else
+        if (4 > 5)
+        {
+
+        }
+        else
+        {
+
+        }
+        //if ... else if ... else
+        if (4 > 5)
+        {
+
+        }
+        else if (6 < 5)
+        {
+
+        }
+        else
+        {
+
+        }
+        // switch
+        int valor = 1;
+        switch (valor)
+        {
+            case 0:
+                break;
+            default:
+                break;
+        } */
+
+        // operadores
+        // logicos: and(&& &) or(|| |) not(!)
+        // Matematicos: + - * / % 
+        // Comparacion: < > != <= >= ==
+        // Asignacion: =
     }
 }
