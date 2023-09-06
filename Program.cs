f47f971af3e9abd930aa8cdfaa727d4155b0b353
duvanrg <135860225+duvanrg@users.noreply.github.com>
2023-09-06 12:00:11 -0500

    feat: :zap: Class 6 sept
    class for class and atributtes
    
    @duvanrg

--- Program.cs
+++ Program.cs
@@ -5,108 +5,54 @@ internal class Program
 
     private static void Main(string[] args)
     {
-        // ArrayList Carnivoros = new ArrayList();
-        // ArrayList Acuaticos = new ArrayList();
-        // ArrayList Animales = new ArrayList();
-        // Carnivoros.Add("Tigre");
-        // Carnivoros.Add("Leon");
-        // Carnivoros.Add("Pantera");
-        // Carnivoros.Add("Leopardo");
-        // Acuaticos.Add("Tiburon");
-        // Acuaticos.Add("Delfin");
-        // Acuaticos.Add("Ballena");
-        // Acuaticos.Add("Pulpo");
-        // Animales.AddRange(Carnivoros);
-        // foreach (Object n in Animales)
-        // {
-        //     Console.WriteLine(n.ToString());
-        // }
-        // Console.WriteLine("Presiona Enter para continuar");
-        // Console.ReadLine();
-        // Console.WriteLine("===================================");
-
-        // Console.Clear();
-        // Animales.AddRange(Acuaticos);
-        // foreach (Object n in Animales)
-        // {
-        //     Console.WriteLine(n.ToString());
-        // }
-
-        // string? palabra;
-        // int idx;
-        // ArrayList Materias = new ArrayList();
-        // ArrayList Cursos = new ArrayList();
-
-        // Materias.Add("Calculo");
-        // Materias.Add("Español");
-        // Materias.Add("Dibujo Tecnico");
-        // Materias.Add("Ingles");
-
-        // Cursos.Add("Octavo");
-        // Cursos.Add("Noveno");
-
-        // Console.Write("Escriba una materia a buscar: ");
-        // palabra = Console.ReadLine();
-        // Console.WriteLine(Materias.Contains(palabra) ? "La materia esta disponible" : "La materia no se encontro");
-        // Console.WriteLine(Materias.IndexOf(palabra));
-
-        // idx = Materias.LastIndexOf(palabra);
-        // Console.WriteLine($"La asignatura {palabra} se encontro en la ultima posicion {idx}");
-
-
-        // do
-        // {
-        //     Console.Write("Ingrese la posicion donde desea insertar la Asignatura: ");
-        //     idx = Convert.ToInt32(Console.ReadLine()); 
-        // } while (idx > Materias.Count);
-        // Materias.InsertRange(idx, Cursos);
-
-        // Console.Write("Excribe una materia para insertar: ");
-        // palabra = Console.ReadLine();
-        // Materias.Insert(idx, palabra);
-
-        // foreach (Object n in Materias)
-        // {
-        //     Console.WriteLine(n.ToString());
-        // }
-
-
-        // ArrayList miArrayList = new ArrayList();
-        // miArrayList.Add("Manzana");
-        // miArrayList.Add("Banana");
-        // miArrayList.Add("Naranja");
-
-        // for (var i = 0; i < 10; i++)
-        // {
-        //     miArrayList.Add(i);
-        // }
-        // miArrayList.RemoveAt(1);
-
-        // String[] myArray = (String[])miArrayList.ToArray(typeof(string));
-
-        // foreach (Object n in myArray)
-        // {
-        //     Console.WriteLine(n);
-        // }
-
-        // foreach (Object n in miArrayList)
-        // {
-        //     Console.WriteLine(n.ToString());
-        // }
-
-
-        ArrayList Materias =  new ArrayList();
-        String[] especialidad =  {"Sistemas", "Mecatronica", "Industrial", "Gestion Empresarial", "Energiar Renovables", "Tecnologias de la Informacion"};
-
-        foreach (Object dato in  Materias)
-        {
-            if(dato.GetType() == typeof(System.String[])){
-                foreach (String dato2 in (String[]) dato)
-                {
-                    Console.WriteLine(dato2);
-                    
-                }
-            }
+        Console.Clear();
+        int idx;
+        List<String> Materias = new List<String>();
+        Materias.Add("Calculo");
+        Materias.Add("Español");
+        Materias.Add("Dibujo Tecnico");
+        Materias.Add("Ingles");
+        ContarLista(Materias);
+        // ############################################################
+        Console.WriteLine("Ingrese la posicion: ");
+        idx = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"El elemento en el indice {idx} es:  {ObtenerElemento(idx, Materias)}");
+        // ############################################################
+        Materias.Add(AddItemData());
+        // ############################################################
+        Console.WriteLine("Ingrese la palabra a buscar: ");
+        if (ValidarItem(Materias, Console.ReadLine())){
+            Console.WriteLine("Elemento encontrado");
+        }else{
+            Console.WriteLine("Elemento no encontrado");
         }
+        // ############################################################
+        Console.ReadKey();
+    }
+
+    public static String AddItemData()
+    {
+        Console.WriteLine("Ingrese el titulo del elemento: ");
+        return Console.ReadLine();        
+    }
+
+    public static void ContarLista(List<String> lista)
+    {
+        Console.WriteLine($"Total de elementos de la lista {lista.Count()}");
+    }
+
+    /// <summary>
+    /// Metodo para obtener el elemento de una posicion de una lista generica
+    /// </summary>
+    /// <param name="idx">Indice de la posicion a obtener</param>
+    /// <param name="lista">Coleccion generica de los datos</param>
+    /// <returns></returns>
+    public static String ObtenerElemento(int idx, List<String> lista)
+    {
+        return lista[idx];
+    }
+
+    public static bool ValidarItem(List<String> lista, string valor){
+        return lista.Exists(l => l.Equals(valor));
     }
 }
